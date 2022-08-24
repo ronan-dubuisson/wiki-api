364250cef94ba0fffdc584a9b2580e8b4adbcfce
Ronan Dubuisson <ronandubuisson@Ronans-MacBook-Pro.local>
2022-08-24 15:58:59 +0200

    finished base set up of project

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('ejs');
+
+const dbConnectionString = 'mongodb://localhost:27017/wikiDB';
 
 const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello world');
